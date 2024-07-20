57928f309d9e831bf947c9f5ae7752d56192428c
Alixspirit <166947928+Alixspirit@users.noreply.github.com>
2024-07-20 16:13:43 +0200

    Create Checkpoint2_Exercice2.ps1

---
+++ Checkpoint2_Exercice2.ps1
@@ -0,0 +1,6 @@
+Q.2.1 :
+Pour transferer le dossier Scripts du serveur vers le client, il faut activer le support de partage de fichier SMB 1.0/CIFS. 
+Pour l'activer, il faut aller dans Panneau de configuration, cliquer sur Applications, cliquer sur Fonctionnalités facultativees et cliquer sur Plus de fonctionnalitées windows.
+Dans la fenetre Activer ou desactiver des fonctionnalitées windows, cliquer sur Support de partage de fichiers SMB 1.0/CIFS.
+
+https://github.com/Alixspirit/Checkpoint2/issues/9#issue-2420915297
